efa46ab09bd9dfb6b76df709515d7e34d1c081cb
reng99 <1837895991@qq.com>
2018-04-08 15:36:29 +0800

    achieve project

--- fis-conf.js
+++ fis-conf.js
@@ -8,3 +8,23 @@ fis.set('project.ignore', [
 	'fis-conf.js',
 	'README.md'
 ]);
+
+fis.match('*.js', {
+	// fis-optimizer-uglify-js 插件进行压缩，已内置
+	optimizer: fis.plugin('uglify-js')
+});
+
+fis.match('*.css', {
+	// fis-optimizer-clean-css 插件进行压缩，已内置
+	optimizer: fis.plugin('clean-css')
+});
+
+fis.match('*.png', {
+	// fis-optimizer-png-compressor 插件进行压缩，已内置
+	optimizer: fis.plugin('png-compressor')
+});
+
+// 文件指纹
+fis.match('*.{js,css,png,jpeg,jpg,gif}', {
+	useHash: true
+});
